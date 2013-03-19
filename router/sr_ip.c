3e90b0da899fae89e2c7f31cf3663f99d2a17725
mininet <timzwang@gmail.com>
2013-03-19 06:01:32 -0700

    Updated names in sr_ip for cleanliness
    - Removed minlength from function call

--- router/sr_ip.c
+++ router/sr_ip.c
@@ -19,99 +19,100 @@
 #include "sr_utils.h"
 #include "sr_router.h"
 
-/* Helpers */
 
-int sr_process_ip_packet(struct sr_instance* sr, uint8_t * packet, unsigned int len, int minlength, char* iface) {
-  minlength += sizeof(sr_ip_hdr_t);
-  if (len < minlength) {
+int sr_process_ip_packet(struct sr_instance* sr,uint8_t* packet, unsigned int len, char* iface) {
+
+  /* Start of next header: add to packet head */
+  unsigned int next_hdr = sizeof(sr_ethernet_hdr_t);
+
+  if (len < next_hdr + sizeof(sr_ip_hdr_t)) {
     fprintf(stderr, "IP header: insufficient length\n");
     return -1;
   }
 
-  print_hdr_ip(packet + sizeof(sr_ethernet_hdr_t));
+  printf("*** -> Processing IP Packet\n");
+  print_hdr_ip(packet + next_hdr);
 
-  /* Create IP Packet */
-  sr_ip_hdr_t *iphdr = (sr_ip_hdr_t *)(packet + sizeof(sr_ethernet_hdr_t));
+  /* Create request IP Packet */
+  sr_ip_hdr_t *req_ip = (sr_ip_hdr_t *)(packet + next_hdr);
 
-  uint16_t ip_checksum = iphdr->ip_sum;
-  iphdr->ip_sum = 0;
+  uint16_t req_cksum = req_ip->ip_sum;
+  req_ip->ip_sum = 0;
 
-  if (cksum(packet + sizeof(sr_ethernet_hdr_t), sizeof(sr_ip_hdr_t)) != ip_checksum) {
+  if (cksum(packet + next_hdr, sizeof(sr_ip_hdr_t)) != req_cksum) {
     fprintf(stderr, "IP: invalid checksum\n");
     return -1;
   }
 
   /* Check if in router's interfaces */
-  struct sr_if* own_interface = sr_find_interface(sr, iphdr->ip_dst);
+  struct sr_if* own_interface = sr_find_interface(sr, req_ip->ip_dst);
 
   if (own_interface) {
     /* Interface exists */
+    next_hdr += sizeof(sr_ip_hdr_t);
 
     /* Initialize response */
     unsigned int response_length = 0;
     uint8_t* response_packet = 0;
 
-    if (iphdr->ip_p == ip_protocol_icmp) { /* ICMP */
-      minlength += sizeof(sr_icmp_hdr_t);
-      if (len < minlength) {
+    if (req_ip->ip_p == ip_protocol_icmp) { /* ICMP */
+      if (len < next_hdr + sizeof(sr_icmp_hdr_t)) {
         fprintf(stderr, "ICMP header: insufficient length\n");
         return -1;
       }
 
       /* Create ICMP Packet */
-      sr_icmp_hdr_t * icmphdr = (sr_icmp_hdr_t *)(packet + sizeof(sr_ethernet_hdr_t) + sizeof(sr_ip_hdr_t));
+      sr_icmp_hdr_t * req_icmp = (sr_icmp_hdr_t *)(packet + next_hdr);
 
-      uint16_t icmp_checksum = icmphdr->icmp_sum;
-      icmphdr->icmp_sum = 0;
+      uint16_t req_icmp_cksum = req_icmp->icmp_sum;
+      req_icmp->icmp_sum = 0;
 
-      if (cksum(packet + sizeof(sr_ethernet_hdr_t) + sizeof(sr_ip_hdr_t), len - sizeof(sr_ethernet_hdr_t) - sizeof(sr_ip_hdr_t)) != icmp_checksum) {
+      if (cksum(packet + next_hdr, len - next_hdr) != req_icmp_cksum) {
         fprintf(stderr, "ICMP: invalid checksum\n");
         return -1;
       }
 
       /* Process ICMP message */
-      printf("ICMP (type %d, code %d)\n", icmphdr->icmp_type, icmphdr->icmp_code);
+      printf("ICMP (type %d, code %d)\n", req_icmp->icmp_type, req_icmp->icmp_code);
 
-      if (icmphdr->icmp_type != 8 || icmphdr->icmp_code != 0) {
+      if (req_icmp->icmp_type != 8 || req_icmp->icmp_code != 0) {
         /* Drop packet if not echo request */
         return -1;
       }
 
+      /* Set response length equal to request's */
       response_length = len;
 
       /* Use request packet as response: saves alloc - tim */
       response_packet = packet;
 
       /* Format echo reply */
-      icmphdr->icmp_type = 0;
+      req_icmp->icmp_type = 0;
 
       /* Generate ICMP checksum */
-      icmphdr->icmp_sum = cksum(
-          response_packet + sizeof(sr_ethernet_hdr_t) + sizeof(sr_ip_hdr_t),
-          len - sizeof(sr_ethernet_hdr_t) - sizeof(sr_ip_hdr_t));
+      req_icmp->icmp_sum = cksum(response_packet + next_hdr,
+                                 len - next_hdr);
 
-    } else if (iphdr->ip_p == 6 || iphdr->ip_p == 17) {
+    } else if (req_ip->ip_p == 6 || req_ip->ip_p == 17) {
       /* TCP or UDP */
 
-      response_length = sizeof(sr_ethernet_hdr_t) +
-                                     sizeof(sr_ip_hdr_t) +
-                                     sizeof(sr_icmp_t3_hdr_t);
+      response_length = next_hdr + sizeof(sr_icmp_t3_hdr_t);
 
       /* Create ethernet packet with ICMP Type 3 */
       response_packet = (uint8_t *)malloc(response_length);
 
       /* Populate ICMP Message */
       sr_icmp_t3_hdr_t* response_icmp = (sr_icmp_t3_hdr_t *)(response_packet +
-                                                             sizeof(sr_ethernet_hdr_t) +
-                                                             sizeof(sr_ip_hdr_t));
+                                                             next_hdr);
       response_icmp->icmp_type = 3;
       response_icmp->icmp_code = 3;
-      memcpy(response_icmp->data, iphdr, ICMP_DATA_SIZE);  /* IP Header + 8 bytes */
+
+      /* Copy over IP Header + 8 bytes */
+      memcpy(response_icmp->data, req_ip, ICMP_DATA_SIZE);
 
       /* Generate ICMP checksum */
-      response_icmp->icmp_sum = cksum(
-          response_packet + sizeof(sr_ethernet_hdr_t) + sizeof(sr_ip_hdr_t),
-          sizeof(sr_icmp_t3_hdr_t));
+      response_icmp->icmp_sum = cksum(response_packet + next_hdr,
+                                      sizeof(sr_icmp_t3_hdr_t));
 
     } else {
       /* Drop packet if other protocol */
@@ -123,7 +124,7 @@ int sr_process_ip_packet(struct sr_instance* sr, uint8_t * packet, unsigned int 
                                                sizeof(sr_ethernet_hdr_t));
 
     /* Swap src and dst addresses */
-    response_ip->ip_dst = iphdr->ip_src;
+    response_ip->ip_dst = req_ip->ip_src;
     response_ip->ip_src = own_interface->ip;
 
     /* Generate IP checksum */
@@ -151,7 +152,7 @@ int sr_process_ip_packet(struct sr_instance* sr, uint8_t * packet, unsigned int 
     printf("*** -> Forwarding packet\n");
 
     /* Routing Table lookup */
-    struct sr_rt* route = sr_find_rt_entry(sr, iphdr->ip_dst);
+    struct sr_rt* route = sr_find_rt_entry(sr, req_ip->ip_dst);
     struct sr_arpcache* arp_cache = &sr->cache;
     struct sr_arpentry* arp_entry = sr_arpcache_lookup(arp_cache, route->gw.s_addr);
 
@@ -171,6 +172,7 @@ int sr_process_ip_packet(struct sr_instance* sr, uint8_t * packet, unsigned int 
 
   return 0;
 }
+
 
 int sr_forward_eth_packet(struct sr_instance* sr, uint8_t* packet, unsigned int len, unsigned char* mac, char* iface) {
   /* Set fields in Ethernet pack for quick forwarding and send */
--- router/sr_router.c
+++ router/sr_router.c
@@ -83,7 +83,7 @@ void sr_handlepacket(struct sr_instance* sr,
   print_hdr_eth(packet);  /* DEBUG */
 
   int minlength = sizeof(sr_ethernet_hdr_t);
-  if (len < minlength) {
+  if (len < sizeof(sr_ethernet_hdr_t)) {
     fprintf(stderr, "ETHERNET header: insufficient length\n");
     return;
   }
@@ -91,7 +91,7 @@ void sr_handlepacket(struct sr_instance* sr,
   uint16_t ethtype = ethertype(packet);
 
   if (ethtype == ethertype_ip) { /* IP */
-    if (sr_process_ip_packet(sr, packet, len, minlength, interface)  == -1) {
+    if (sr_process_ip_packet(sr, packet, len, interface)  == -1) {
       fprintf(stderr, "There was an error processing the IP packet\n");
     }
   }
