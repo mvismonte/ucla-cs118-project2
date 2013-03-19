52a36afa7cc4c1242e1c6a93a265820b4408d536
Mark Vismonte <mark.vismonte@gmail.com>
2013-03-19 02:45:14 -0700

    Added for loop in arp cache

--- router/sr_arp.c
+++ router/sr_arp.c
@@ -30,7 +30,18 @@ int sr_process_arp_packet(struct sr_instance* sr, uint8_t *packet, unsigned int 
   sr_arp_hdr_t* arp_hdr = (sr_arp_hdr_t*)(packet);
   struct sr_if* interface = sr_find_interface(sr, arp_hdr->ar_tip);
 
-  /* TODO(mark): Check ARP queue to see if the request is in there. */
+  /* TODO(mark): Check ARP queue to see if the request is in there.
+   The ARP reply processing code should move entries from the ARP request
+   queue to the ARP cache:
+
+   # When servicing an arp reply that gives us an IP->MAC mapping
+   req = arpcache_insert(ip, mac)
+
+   if req:
+       send all packets on the req->packets linked list
+       arpreq_destroy(req)
+  */
+
 
   if (interface) {
     sr_print_if(interface);
@@ -64,5 +75,17 @@ int sr_process_arp_packet(struct sr_instance* sr, uint8_t *packet, unsigned int 
 }
 
 int sr_handle_arpreq(struct sr_instance* sr, struct sr_arpreq* req) {
+  /*
+  Pseudocode
+ if difftime(now, req->sent) > 1.0
+   if req->times_sent >= 5:
+       send icmp host unreachable to source addr of all pkts waiting
+         on this request
+       arpreq_destroy(req)
+   else:
+       send arp request
+       req->sent = now
+       req->times_sent++
+  */
   return 0;
 }
